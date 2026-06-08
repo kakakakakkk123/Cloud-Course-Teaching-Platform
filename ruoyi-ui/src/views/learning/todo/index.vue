3653b29720127327319bd53fc293823bd3b4134d
kakakakakkk123 <3530681286@qq.com>
2026-06-08 11:47:47 +0800

    fix: 隐藏学生待办原始编辑区

--- ruoyi-ui/src/views/learning/todo/index.vue
+++ ruoyi-ui/src/views/learning/todo/index.vue
@@ -9,6 +9,7 @@
     placeholder="可以直接记录待办文本，也可以保存 JSON 列表。示例：[{'title':'完成数据库作业','content':'提交实验三','time':'2026-06-07'}]"
     :allow-quick-add="true"
     quick-add-button-text="添加学习任务"
+    :show-raw-editor="false"
   />
 </template>
 
