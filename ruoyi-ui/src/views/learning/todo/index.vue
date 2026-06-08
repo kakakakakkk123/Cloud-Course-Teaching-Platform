b8b194d5ef109152b5f69f57c963eefda1be7602
kakakakakkk123 <3530681286@qq.com>
2026-06-08 11:40:57 +0800

    feat: allow adding learning todo tasks

--- ruoyi-ui/src/views/learning/todo/index.vue
+++ ruoyi-ui/src/views/learning/todo/index.vue
@@ -7,6 +7,8 @@
     empty-text="暂无学习待办"
     description="集中维护课程任务、实验作业、复习计划和阶段目标。"
     placeholder="可以直接记录待办文本，也可以保存 JSON 列表。示例：[{'title':'完成数据库作业','content':'提交实验三','time':'2026-06-07'}]"
+    :allow-quick-add="true"
+    quick-add-button-text="添加学习任务"
   />
 </template>
 
